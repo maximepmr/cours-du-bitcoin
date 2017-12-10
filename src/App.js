4146349af5f352958d629e84016412de527dd51a
maxim <maxime.pomier@supersoluce.com>
2017-12-10 17:42:17 +0100

    msg informatif

--- src/App.js
+++ src/App.js
@@ -67,6 +67,7 @@ class App extends Component {
           <div className="bitcoinprice">
               {this.state.bitCoinPrice} €
           </div>
+            {/*charlotte je t'aime*/}
           <div className="maxime">
               maxime@pomier.fr
           </div>
